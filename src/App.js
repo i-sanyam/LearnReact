293b82d3b43cb18f9c3f818518ddb319817cbf22
i-sanyam <sanyam.nitj@gmail.com>
2020-11-01 15:52:37 +0530

    state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import ToDo from './components/ToDo'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <h1>Hello World</h1> */}
-      <ToDo />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1, 
+        task: "Laundry"
+      },
+      {
+        id: 2, 
+        task: "Dishes"
+      },
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <h1>Hello World</h1> */}
+        <ToDo todos = {this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
