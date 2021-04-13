c1965c1a3800c767a16d5b6dc911d06b05f7b717
i-sanyam <i-sanyam>
2021-04-13 23:55:36 +0530

    configurable children component - added props to components

--- 03/src/CommentDetail.js
+++ 03/src/CommentDetail.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
   return (
     <div className='comment'>
       <a href="/" className="avatar">
-        <img alt='avatar' src={Faker.image.avatar()}></img>
+        <img alt='avatar' src={props.avatarSrc}></img>
       </a>
       <div className='content'>
-        <a href="/" className="author">Stephen</a>
+        <a href="/" className="author">{props.author}</a>
         <div className='metadata'>
-          <span className="date">Today at 6:30PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">This is a comment</div>
+        <div className="text">{props.commentText}</div>
       </div>
     </div>
   );
--- 03/src/index.js
+++ 03/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import Faker from 'faker';
 
 const App = () => {
+  console.log(Faker.date.recent());
   return (
     <div className='ui comments containter'>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        commentText={Faker.lorem.words()}
+        author={Faker.name.firstName()}
+        avatarSrc={Faker.image.avatar()}
+        timeAgo={Faker.date.recent().toString()} />
+      <CommentDetail
+        commentText={Faker.lorem.words()}
+        author={Faker.name.firstName()}
+        avatarSrc={Faker.image.avatar()}
+        timeAgo={Faker.date.recent().toString()} />
+      <CommentDetail
+        commentText={Faker.lorem.words()}
+        author={Faker.name.firstName()}
+        avatarSrc={Faker.image.avatar()}
+        timeAgo={Faker.date.recent().toString()} />
     </div>
   );
 }
