c767ea1ef2c9e82e9b3b5f5541c71f6f1571ecec
i-sanyam <i-sanyam>
2021-04-18 00:03:11 +0530

    fetch location and render using state and this.setState

--- 04-05-Seasons/src/index.js
+++ 04-05-Seasons/src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition((geoPosition) => {
+      this.setState({ lat: geoPosition.coords.latitude });
+    }, console.error);
+  }
   render() {
-  window.navigator.geolocation.getCurrentPosition(console.log, console.log);
-  return (
-    <div>
-      Hello World!
-      <SeasonDisplay />
-    </div>
-  );
+    return (
+      <div>
+        Hello World!
+        <br/>
+      Latitude: {this.state.lat}
+        <SeasonDisplay />
+      </div>
+    );
   }
 }
 
