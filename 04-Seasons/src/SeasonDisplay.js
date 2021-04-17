dcaaec81528281c6346b5f5e5842043c0e18fbff
i-sanyam <i-sanyam>
2021-04-17 19:51:29 +0530

    set up wireframe

---
+++ 04-Seasons/src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+  return (
+    <div>I am the season disply Component!</div>
+  );
+}
+
+export default SeasonDisplay;
---
+++ 04-Seasons/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+
+const App = () => {
+  return (
+    <div>
+      Hello World!
+      <SeasonDisplay />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
