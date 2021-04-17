29dafe9edd2a61f80a161338e611e2417687fd55
i-sanyam <i-sanyam>
2021-04-17 20:14:17 +0530

    have to use class based components this does not work

--- 04-Seasons/src/index.js
+++ 04-Seasons/src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay'
 
 const App = () => {
-  return (
-    <div>
-      Hello World!
-      <SeasonDisplay />
-    </div>
-  );
+  // window.navigator.geolocation.getCurrentPosition(console.log, console.log);
+  // new Date().getMonth();
+  window.navigator.geolocation.getCurrentPosition((pos)=>{
+    return (
+      <div>
+        Hello World! ${pos}
+        <SeasonDisplay />
+      </div>
+    );
+  }, (err)=>{
+    return (
+      <div>
+        Hello World! ${err}
+        <SeasonDisplay />
+      </div>
+    );
+  });
+  // return (
+  //   <div>
+  //     Hello World!
+  //     <SeasonDisplay />
+  //   </div>
+  // );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
