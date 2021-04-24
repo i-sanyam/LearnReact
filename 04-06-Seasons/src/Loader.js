1440959d9c505fb699ad3f76194111c7f64d0c52
i-sanyam <i-sanyam>
2021-04-24 17:23:47 +0530

    add loader component

---
+++ 04-06-Seasons/src/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui text loader">Loading...</div>
+    </div>);
+}
+
+export default Loader;
--- 04-06-Seasons/src/index.js
+++ 04-06-Seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay'
+import Loader from './Loader';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: null };
@@ -28,7 +29,7 @@ class App extends React.Component {
         </div>
       );
     }
-    return <div><i className="icon spinner" /></div>;
+    return <Loader/>
   }
 }
 
