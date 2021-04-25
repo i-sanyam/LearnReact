e845cbc81c5af55d7ebfc56766ea8583a833ec0d
i-sanyam <i-sanyam>
2021-04-25 19:21:02 +0530

    refactor search bar to class based component add on change method

--- 7-10/src/components/SearchBar.js
+++ 7-10/src/components/SearchBar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <div className='ui segment'>
-    <form className = 'ui form'>
-      <div className='field'>
-        <label>Image Search</label>
-        <input type="text"/>
+class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+  render() {
+    return (
+      <div className='ui segment'>
+      <form className = 'ui form'>
+        <div className='field'>
+          <label>Image Search</label>
+            <input type="text" onChange={ this.onInputChange }/>
+        </div>
+      </form>
       </div>
-    </form>
-    </div>
-  );
+    );
+  }
 };
 
 export default SearchBar;
