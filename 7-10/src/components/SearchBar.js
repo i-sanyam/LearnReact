d30eb5ca180b6d33339a81319f3a85cfc8685dcd
i-sanyam <i-sanyam>
2021-04-25 19:43:04 +0530

    You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.     at input     at div

--- 7-10/src/components/SearchBar.js
+++ 7-10/src/components/SearchBar.js
@@ -3,12 +3,18 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: 'Enter your search item' };
   render() {
+    console.log('render');
     return (
       <div className='ui segment'>
         <form className='ui form'>
           <div className='field'>
             <label>Image Search</label>
-            <input type="text" value={ this.state.term} onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} />
+            <input
+              type="text"
+              // value={this.state.term}
+              value = 'Tarun'
+              // onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })}
+            />
           </div>
         </form>
       </div>
