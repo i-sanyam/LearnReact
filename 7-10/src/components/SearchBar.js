149ee7920bab0365fc2a7b53e6192a00f268b491
i-sanyam <i-sanyam>
2021-04-25 22:26:36 +0530

    add on form submit

--- 7-10/src/components/SearchBar.js
+++ 7-10/src/components/SearchBar.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = { term: 'Enter your search item' };
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
   render() {
-    console.log('render');
-    console.log('state', this.state.term);
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form
+          onSubmit={this.onFormSubmit}
+          className='ui form'>
           <div className='field'>
             <label>Image Search</label>
             <input
               type="text"
-              // value={this.state.term}
-              // defaultValue='Tarun'
+              value={ this.state.term }
               onChange={(e) => {
-                console.log('TARGETVALUE', e.target.value);
-                console.log('state', this.state.term);
-                if (e.target.value.length > 4) return;
-                this.setState({ term: e.target.value.toUpperCase() });
+                this.setState({ term: e.target.value });
               }}
             />
           </div>
