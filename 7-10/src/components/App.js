f743fa56ed449f41154189295d22b9b46972167e
i-sanyam <i-sanyam>
2021-04-25 23:13:34 +0530

    call by axios images api

--- 7-10/src/components/App.js
+++ 7-10/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios'
 import SearchBar from './SearchBar.js'
 
 class App extends React.Component {
-  onSearchSubmit = searchQuery => {
+  state = { imageArray: [] };
+  onSearchSubmit = async searchQuery => {
     console.log(searchQuery);
+    try {
+      const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+          Authorization: 'Client-ID czkk0Ad5r44tL-3WLfsnO1I7X_yTVw6GfTcIYEcXpnw'
+        },
+        params: {
+          query: searchQuery
+        }
+      });
+      console.log(response.data.results);
+    } catch (err) {
+      console.error(err);
+    }
   }
   render() {
     return (
