9ca87a5a23c1cff3f36ffded40b803da8fc00b04
i-sanyam <i-sanyam>
2021-04-25 19:22:53 +0530

    change on change method to inline and convert search bar to a controlled on change element

--- 7-10/src/components/SearchBar.js
+++ 7-10/src/components/SearchBar.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
   render() {
     return (
       <div className='ui segment'>
       <form className = 'ui form'>
         <div className='field'>
           <label>Image Search</label>
-            <input type="text" onChange={ this.onInputChange }/>
+            <input type="text" onChange={(e) => this.setState({ term: e.target.value })}/>
         </div>
       </form>
       </div>
