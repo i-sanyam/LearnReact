8069f08e12335cff46d98dfe6ed3a64bc8ee3df0
i-sanyam <i-sanyam>
2021-04-26 00:41:30 +0530

    add grid layout

--- 7-10/src/components/ImageCard.js
+++ 7-10/src/components/ImageCard.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.ImageRef = React.createRef();
+  }
+  handleSpans = () => {
+    const height = this.ImageRef.current.clientHeight;
+    this.setState({ spans: Math.ceil(height / 10) })
+  }
+  componentDidMount() {
+    this.ImageRef.current.addEventListener('load', this.handleSpans);
+  }
   render() {
     const { description, urls } = this.props.image;
-    return <img alt={description} src={urls.regular}/>
+    return <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+      <img ref={this.ImageRef} alt={description} src={urls.regular} />
+    </div>
   }
 }
 
