bcbfae744bf9996891cb54c8834084f20caba3c5
i-sanyam <i-sanyam>
2021-04-26 00:51:53 +0530

    remove unsplash cleint id from project

--- 7-10/src/api/unsplash.js
+++ 7-10/src/api/unsplash.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { UNSPLASH_CLIENT_ID } from './config.json'
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID czkk0Ad5r44tL-3WLfsnO1I7X_yTVw6GfTcIYEcXpnw'
+    Authorization: `Client-ID ${UNSPLASH_CLIENT_ID}`
   }
 });
