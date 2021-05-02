1653a760dd63acb5c4a59bdb32c34062ef3bfbe7
i-sanyam <i-sanyam>
2021-05-02 12:33:32 +0530

    add video list and video item component

--- 11/src/components/App.js
+++ 11/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import youtube from '../api/youtube';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = { videos: [] };
@@ -13,9 +14,12 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   }
   render() {
-    return <div className='ui container'>
-      <SearchBar onSearchSubmit={this.onSearchSubmit} />
-    </div>;
+    return (
+      <div className='ui container'>
+        <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
   }
 }
 
---
+++ 11/src/components/VideoItem.js
@@ -0,0 +1,17 @@
+import './VideoItem.css'
+import React from 'react';
+
+const VideoItem = ({ video }) => {
+  return (
+    <div className="video-item item">
+      <img className='ui image' alt={video.snippet.title} src={video.snippet.thumbnails.default.url} />
+      <div className='content'>
+        <div className='header'>
+          {video.snippet.title}
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default VideoItem;
