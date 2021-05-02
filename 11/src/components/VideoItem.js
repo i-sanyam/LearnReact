6cd8b50f8bc3d65493e87ba44daf0f3a2dccde8c
i-sanyam <i-sanyam>
2021-05-02 13:49:35 +0530

    add video detail component

--- 11/src/components/VideoItem.js
+++ 11/src/components/VideoItem.js
@@ -1,10 +1,13 @@
 import './VideoItem.css'
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
-      <img className='ui image' alt={video.snippet.title} src={video.snippet.thumbnails.default.url} />
+    <div onClick={() => { onVideoSelect(video) }} className="video-item item">
+      <img
+        className='ui image'
+        alt={`Thumbnail for ${video.snippet.title}`}
+        src={video.snippet.thumbnails.default.url} />
       <div className='content'>
         <div className='header'>
           {video.snippet.title}
